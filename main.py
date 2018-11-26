a6745d07d7440e0b6cdc882e3185ae0a41ea5672
Mayank Prasoon <mayank.prasoon.che14@iitbhu.ac.in>
2018-11-27 02:24:16 +0530

    Completed the project

---
+++ main.py
@@ -0,0 +1,68 @@
+import json
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file_location', help="enter json file location", type=str)
+parser.add_argument('rules_txt', help="enter rules file", type=str)
+args = parser.parse_args()
+
+input_json = {}
+
+def set_data(data, input_json):
+	# The function is used to locally get the input json field
+	each_entry = {}
+	each_entry["value_type"] = each_input["value_type"]
+	each_entry["value"] = each_input["value"]
+	if(each_input["signal"] in input_json):
+		input_json[each_input["signal"]].append(each_entry)
+	else:
+		input_json[each_input["signal"]] = []
+		input_json[each_input["signal"]].append(each_entry)
+
+with open(args.file_location) as json_file:
+	# The command is used to get the input signal json file
+	data = json.load(json_file)
+	for each_input in data:
+		set_data(each_input, input_json)
+
+def checkcondition(case, reqd_val, given_signal):
+	# The function is used to check the given value condition
+	old_val = case["value_condition"]
+	if(case["value_type"]=="Integer" and reqd_val["value_type"]=="Integer"):
+		given_value = reqd_val["value"]
+		newval = 'float(reqd_val["value"])'
+		case["value_condition"] = case["value_condition"].replace("value",newval)
+		if(not eval(case["value_condition"])):
+			notsatisfying(case, old_val, given_signal, given_value)
+
+	elif(case["value_type"]=="String" and reqd_val["value_type"]=="String"):
+		given_value = reqd_val["value"]
+		newval = 'reqd_val["value"]'
+		case["value_condition"] = case["value_condition"].replace("value",newval)
+		if(not eval(case["value_condition"])):
+			notsatisfying(case, old_val, given_signal, given_value)
+
+	elif(case["value_type"]=="Datetime" and reqd_val["value_type"]=="Datetime"):
+		given_value = reqd_val["value"]
+		f = "%Y-%m-%d %H:%M:%S"
+		newval = 'datetime.datetime.strptime(reqd_val["value"],f)'
+		case["value_condition"] = case["value_condition"].replace("value",newval)
+		if(not eval(case["value_condition"])):
+			notsatisfying(case, old_val, given_signal, given_value)
+	case["value_condition"] = old_val
+
+def notsatisfying(case, old_val, given_signal, given_value):
+	# The function is used to print the values that dont satisfy the given output
+	print("%s signal is not satisfying the condition %s for %s variable type when the value is %s\n" % (given_signal, old_val, case["value_type"], given_value))
+
+with open(args.rules_txt) as json_file:
+	# The command is used to get the input rules file
+	data = json.load(json_file)
+	for each_rule in data:
+		given_signal = each_rule["signal"]
+		reqd_vals = input_json[given_signal]
+		for reqd_val in reqd_vals:
+			for condition in each_rule["conditions"]:
+				checkcondition(condition, reqd_val, given_signal)
+
