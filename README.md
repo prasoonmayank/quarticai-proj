0c3cf4cb6c15d5bbc84cb2bb6d70daaa3687d915
Mayank Prasoon <mayank.prasoon.che14@iitbhu.ac.in>
2018-11-27 04:52:11 +0530

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# quarticai-proj
+# Quarticai-Proj
+
+## Approach:
+The rules file is such that one can add multiple conditions for the different data types that are present within the signal. Thus, the different rules for the signals will be put in the json. At the same time the input is also stored in a different dictionary format locally. It is stored as :
+
+{
+	"ATL1": {"String":["HIGH","LOW"...],"Integer":["13.64","3.3"...],"Datetime":["2017-05-13 06:22:35", "2017-08-20 07:25:29"...]},
+	"ATL2": {"String":["HIGH","LOW"...],"Integer":["13.64","3.3"...],"Datetime":["2017-05-13 06:22:35", "2017-08-20 07:25:29"...],
+	"ATL3": {"String":["HIGH","LOW"...],"Integer":["13.64","3.3"...],"Datetime":["2017-05-13 06:22:35", "2017-08-20 07:25:29"...],
+	...
+	...
+	...
+}
+
+The rules are stored as :
+
+[
+	{
+		"signal": "ATL1",
+		"conditions": [
+			{
+				"value_type": "Integer",
+				"value_conditions": [
+					"value<=240.00"
+				]
+			}
+		]
+	},
+	{
+		"signal": "ATL2",
+		"conditions": [
+			{
+				"value_type": "String",
+				"value_conditions": [
+					"value!='LOW'"
+				]
+			}
+		]
+	},
+	{
+		"signal": "ATL3",
+		"conditions": [
+			{
+				"value_type": "Datetime",
+				"value_conditions": [
+					"value<=datetime.datetime.now()"
+				]
+			}
+		]
+	}
+]
+
+This ensures that the rules are properly formatted based on the variable_types they apply to.
+The search is really fast in the current saved format, while taking much lower space.
+
+Currently, the worst time complexity of saving the data in the format shown above is O(n x (a+b)) where n is the total number of entered values while a and b are the number of signals and number of values in each data type.
+The worst case time complexity of getting the signal with wrong info is O(a x c x d x e) where a is the number of signals, c is the number of datatypes for which the signal is present(3 in our case), d is the number of values in each type while e is the number of conditions for each type of variable.
+
+There are no bottlenecks to the program. Although, the rules need to be always in the format specified.
+
+If given more time, we can further generalize the program to consider all different types of conditions possible for the different data types other than the String, Integer or the Datetime that is currently given. Also, unlike right now where each of the data types are accessed differently, the code can be further optimized to consider cases where different variables of different types are linked as well.
+
+Thus, if given more time, I would first try to generalize the code for all different data types. Followed by trying to consider two different variable comparision under the same rule.
+
+## Running the code
+To run the program: python main.py raw_data.json rules.json
+The program has been made on Python 3.7.0
+
+## Output of the code
+The code gives the output in the following format:
+
+"[SIGNAL_CODE] signal is not satisfying the condition [CONDITION] for [VARIABLE TYPE CONSIDERED] variable type when the value is [VARIABLE VALUE]\n"
--- main.py
+++ main.py
@@ -1,24 +1,28 @@
+
+# Importing the standard libraries
 import json
 import argparse
 import datetime
 
+# Specifying the command line parsers
 parser = argparse.ArgumentParser()
 parser.add_argument('file_location', help="enter json file location", type=str)
 parser.add_argument('rules_txt', help="enter rules file", type=str)
 args = parser.parse_args()
 
+# The input json variable after formatting
 input_json = {}
 
-def set_data(data, input_json):
+def set_data(each_input, input_json):
 	# The function is used to locally get the input json field
-	each_entry = {}
-	each_entry["value_type"] = each_input["value_type"]
-	each_entry["value"] = each_input["value"]
-	if(each_input["signal"] in input_json):
-		input_json[each_input["signal"]].append(each_entry)
+	if each_input["signal"] in input_json:
+		each_entry = input_json[each_input["signal"]]
 	else:
-		input_json[each_input["signal"]] = []
-		input_json[each_input["signal"]].append(each_entry)
+		each_entry = {}
+	if(not each_input["value_type"] in each_entry):
+		each_entry[each_input["value_type"]] = []
+	each_entry[each_input["value_type"]].append(each_input["value"])
+	input_json[each_input["signal"]] = each_entry
 
 with open(args.file_location) as json_file:
 	# The command is used to get the input signal json file
@@ -26,39 +30,30 @@ with open(args.file_location) as json_file:
 	for each_input in data:
 		set_data(each_input, input_json)
 
-def checkcondition(case, reqd_val, given_signal):
-	# The function is used to check the given value condition
-	if(case["value_type"]=="Integer" and reqd_val["value_type"]=="Integer"):
-		given_value = reqd_val["value"]
-		newval = 'float(reqd_val["value"])'
-		for given_condition in case["value_conditions"]:
-			unchanged_condition = given_condition
-			given_condition = given_condition.replace("value",newval)
-			if(not eval(given_condition)):
-				notsatisfying(case, unchanged_condition, given_signal, given_value)
+def checkcondition(given_signal, valtype, eachtypevalue, given_val_condition):
+	# This part of the code is used to check the different values for the given conditions
+	old_condition = given_val_condition
+	if(valtype=="Integer"):
+		val_condition = given_val_condition.replace("value","float(eachtypevalue)")
+		if(not eval(val_condition)):
+			notsatisfying(given_signal, old_condition, valtype, eachtypevalue)
 
-	elif(case["value_type"]=="String" and reqd_val["value_type"]=="String"):
-		given_value = reqd_val["value"]
-		newval = 'reqd_val["value"]'
-		for given_condition in case["value_conditions"]:
-			unchanged_condition = given_condition
-			given_condition = given_condition.replace("value", newval)
-			if(not eval(given_condition)):
-				notsatisfying(case, unchanged_condition, given_signal, given_value)
+	elif(valtype=="String"):
+		val_condition = given_val_condition.replace("value","eachtypevalue")
+		if(not eval(val_condition)):
+			notsatisfying(given_signal, old_condition, valtype, eachtypevalue)
 
-	elif(case["value_type"]=="Datetime" and reqd_val["value_type"]=="Datetime"):
-		given_value = reqd_val["value"]
+	elif(valtype=="Datetime"):
 		f = "%Y-%m-%d %H:%M:%S"
-		newval = 'datetime.datetime.strptime(reqd_val["value"],f)'
-		for given_condition in case["value_conditions"]:
-			unchanged_condition = given_condition
-			given_condition = given_condition.replace("value", newval)
-			if(not eval(given_condition)):
-				notsatisfying(case, unchanged_condition, given_signal, given_value)
+		eachtypevalue = datetime.datetime.strptime(eachtypevalue,f)
+		val_condition = given_val_condition.replace("value","eachtypevalue")
+		if(not eval(val_condition)):
+			notsatisfying(given_signal, old_condition, valtype, eachtypevalue)
 
-def notsatisfying(case, old_val, given_signal, given_value):
+def notsatisfying(given_signal, old_condition, valtype, eachtypevalue):
 	# The function is used to print the values that dont satisfy the given output
-	print("%s signal is not satisfying the condition %s for %s variable type when the value is %s\n" % (given_signal, old_val, case["value_type"], given_value))
+	print("%s signal is not satisfying the condition %s for %s variable type when the value is %s\n" % (given_signal, old_condition, valtype, eachtypevalue))
+
 
 with open(args.rules_txt) as json_file:
 	# The command is used to get the input rules file
@@ -66,7 +61,10 @@ with open(args.rules_txt) as json_file:
 	for each_rule in data:
 		given_signal = each_rule["signal"]
 		reqd_vals = input_json[given_signal]
-		for reqd_val in reqd_vals:
-			for condition in each_rule["conditions"]:
-				checkcondition(condition, reqd_val, given_signal)
-
+		for condition in each_rule["conditions"]:
+			valtype = condition["value_type"]
+			given_value_conditions = condition["value_conditions"]
+			for eachtypevalues in reqd_vals[valtype]:
+				for given_value_condition in given_value_conditions:
+					checkcondition(given_signal, valtype, eachtypevalues, given_value_condition)
+		
