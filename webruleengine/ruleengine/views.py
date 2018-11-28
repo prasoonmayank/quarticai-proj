33af01d07686b640a75a96aeaae14ced08f7a3fb
Mayank Prasoon <mayank.prasoon.che14@iitbhu.ac.in>
2018-11-28 13:29:37 +0530

    Added the rule engine web page

--- webruleengine/ruleengine/views.py
+++ webruleengine/ruleengine/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
 
 # Create your views here.
+def index(request):
+	context = {}
+	return render(request, 'ruleengine/index.html', context)
